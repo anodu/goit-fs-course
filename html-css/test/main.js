82c04d5f08a5a2382ef7dd1d97cbc59289dd7512
anodu <mil_doc@icloud.com>
2018-12-10 13:11:39 +0200

    kj
    k

---
+++ html-css/test/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// const name = 'mango';
+// const gest=name;
+// console.log(gest);
+
+
+const day= '02';
+const month = '10';
+const year = '2017';
+const date=day+ '/' + month + '/' + year;
+console.log(date)
